c0c92a717570ec6f0bf3c31dd828cc7b4ca53833
Erwan DAYOT <pro.e.dayot@gmail.com>
2023-07-21 11:57:30 +0200

    all particular case

--- universalblockplacer.py
+++ universalblockplacer.py
@@ -38,13 +38,27 @@ for block in blocklist:
         different_block_ids.append(block)
 
 
-unwanted_blocks=[]
+unwanted_blocks=[
+    "bubble_column",
+    "cave_air",
+    "end_gateway",
+    "end_portal",
+    "fire",
+    "soul_fire",
+    "frosted_ice",
+    "moving_piston",
+    "nether_portal",
+    "piston_head",
+    "tall_seagrass",
+    "void_air",
+    
+]
 
-unwanted_endswith=["_stem","_cauldron","_cake"]
+unwanted_endswith=["_stem","_cauldron","_cake","_plant"]
 unwanted_startwith=["potted_"]
 
 for block in different_block_ids:
-    if (block.endswith("_sign") or block.endswith("_banner") or block.endswith("_fan") or block.endswith("_head") or block.endswith("_torch")) and block.replace("wall_","") in itemlist:
+    if (block.endswith("_sign") or block.endswith("_banner") or block.endswith("_fan") or block.endswith("_head") or block.endswith("_torch") or block.endswith("_skull")) and block.replace("wall_","") in itemlist:
         item_to_blocks[block.replace("wall_","")]["blocks"][block]=blockstates[block]
     elif block.endswith("s") and block[0:-1] in itemlist:
         if block[0:-1] in item_to_blocks:
@@ -64,12 +78,85 @@ for block in different_block_ids:
                     block: blockstates[block]
                 }
             }
+    elif block.endswith("_crop") and block.replace("_crop","_pod") in itemlist:
+        if block.replace("_crop","_pod") in item_to_blocks:
+            item_to_blocks[block.replace("_crop","_pod")]["blocks"][block]=blockstates[block]
+        else:
+            item_to_blocks[block.replace("_crop","_pod")]={
+                "blocks": {
+                    block: blockstates[block]
+                }
+            }
+    elif block.endswith("_crop") and block.replace("_crop","_seeds") in itemlist:
+        if block.replace("_crop","_seeds") in item_to_blocks:
+            item_to_blocks[block.replace("_crop","_seeds")]["blocks"][block]=blockstates[block]
+        else:
+            item_to_blocks[block.replace("_crop","_seeds")]={
+                "blocks": {
+                    block: blockstates[block]
+                }
+            }
+
+
+    # Special cases
     elif block=="redstone_wire":
         item_to_blocks["redstone"]={
             "blocks": {
                 block: blockstates[block]
             }
         }
+    elif block=="bamboo_sapling":
+        item_to_blocks["bamboo"]={
+            "blocks": {
+                block: blockstates["bamboo"]
+            }
+        }
+    elif block=="lava":
+        item_to_blocks["lava_bucket"]={
+            "blocks": {
+                block: blockstates[block]
+            }
+        }
+    elif block=="water":
+        item_to_blocks["water_bucket"]={
+            "blocks": {
+                block: blockstates[block]
+            }
+        }
+    elif block=="cave_vines":
+        item_to_blocks["glow_berries"]={
+            "blocks": {
+                block: blockstates[block]
+            }
+        }
+    elif block=="cocoa":
+        item_to_blocks["cocoa_beans"]={
+            "blocks": {
+                block: blockstates[block]
+            }
+        }
+    elif block=="powder_snow":
+        item_to_blocks["powder_snow_bucket"]={
+            "blocks": {
+                block: {}
+            }
+        }
+    elif block=="tripwire":
+        item_to_blocks["string"]={
+            "blocks": {
+                block: blockstates[block]
+            }
+        }
+    elif block=="sweet_berry_bush":
+        item_to_blocks["sweet_berries"]={
+            "blocks": {
+                block: blockstates[block]
+            }
+        }
+        
+   
+
+
     elif block in unwanted_blocks or any([block.endswith(ending) for ending in unwanted_endswith]) or any([block.startswith(start) for start in unwanted_startwith]):
         pass
     else:
